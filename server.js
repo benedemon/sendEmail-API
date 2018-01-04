f0038003f513d25701a6b8767a039b7866621c0c
benedemon <rajsingh1996@gmail.com>
2018-01-05 00:19:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var nodemailer = require('nodemailer')
+var app = express()
+
+var smtpTransport = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    type: 'OAuth2',
+    user: 'user@address.com',
+    clientId: 'XXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com',
+    clientSecret: 'XXXXXXX-XXXXXXXXXXXXXXXX',
+    refreshToken: 'X/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+  }
+})
+
+app.get('/send', function (req, res) {
+  var mailOptions = {
+    from: 'Email API <benemailapi@gmail.com',
+    to: req.query.to,
+    subject: req.query.subject,
+    text: req.query.text
+  }
+
+  console.log(mailOptions)
+  smtpTransport.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(error)
+      res.end('Error')
+    } else {
+      console.log('Message sent : ', response)
+      res.end('Email Sent')
+    }
+  })
+})
+
+app.listen(3000, function () {
+  console.log('Express Started on Port 3000')
+})
